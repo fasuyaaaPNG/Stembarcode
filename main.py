5153e7c8fcca46d81611f857951a651fa706b000
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-11-28 12:34:39 +0700

    test

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import cv2
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+import streamlit as st
 
-faceCascade = cv2.CascadeClassifier(cv2.haarcascades+'haarcascade_frontalface_default.xml')
+st.title("Webcam Live Feed")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
+camera = cv2.VideoCapture(0)
 
-
-class VideoTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.i = 0
-
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-        i =self.i+1
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (95, 207, 30), 3)
-            cv2.rectangle(img, (x, y - 40), (x + w, y), (95, 207, 30), -1)
-            cv2.putText(img, 'F-' + str(i), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
-
-        return img
-
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+while run:
+    _, frame = camera.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
